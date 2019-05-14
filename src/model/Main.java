7df30a91a859383febce7033822bc20822ee27d3
LutingWang <2457348692@qq.com>
2019-05-14 14:16:29 +0800

    added files

---
+++ src/model/Main.java
@@ -0,0 +1,32 @@
+package model;
+
+import com.oocourse.specs3.AppRunner;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+
+public class Main {
+    public static final boolean DEBUG = false;
+    private static final boolean REDIR = false;
+    private static final boolean TIMER_ON = false; // TODO: turn off
+    private static long timer;
+    
+    public static void main(String[] args) throws Exception {
+        if (REDIR) {
+            System.setIn(new FileInputStream("./test/test.txt"));
+            System.setOut(new PrintStream(
+                    new FileOutputStream("./test/out.txt")));
+        }
+        if (TIMER_ON) {
+            timer = System.currentTimeMillis();
+        }
+        AppRunner runner = AppRunner
+                .newInstance(MyPath.class, MyRailwaySystem.class);
+        runner.run(args);
+        if (TIMER_ON) {
+            System.out.println("Timer: executed "
+                    + (System.currentTimeMillis() - timer) + "ms.");
+        }
+    }
+}
